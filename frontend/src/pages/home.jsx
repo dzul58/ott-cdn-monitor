24fca1da73e6efd8419cc1e4e958a7adbd07f090
dzul58 <dzulfiqar5819@gmail.com>
2024-12-24 14:10:26 +0700

    set up home

---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// import Swal from 'sweetalert2';
+
+const Home = () => {
+
+
+  return (
+    <div className="p-2 bg-slate-50">
+
+    </div>
+  );
+};
+
+export default Home;
