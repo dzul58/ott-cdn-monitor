56ee4aaccf12235441ff48db35998fc416d7c37e
dzul58 <dzulfiqar5819@gmail.com>
2024-12-30 17:46:59 +0700

    changed vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Dengarkan di semua alamat (termasuk IP publik)
+    port: 5173,      // Port default Vite (atau ganti sesuai kebutuhan)
+    strictPort: true // Pastikan Vite gagal jika port sudah digunakan
+  }
+});
